efcab77d37d9cf3b910626881f7869a88cd5aff3
Darrell Tan Wei Hoe <110805869+DarrellTan@users.noreply.github.com>
2025-07-14 15:19:49 +0800

    Second Commit

---
+++ CPRMannequinARTests/CPRMannequinARTests.swift
@@ -0,0 +1,17 @@
+//
+//  CPRMannequinARTests.swift
+//  CPRMannequinARTests
+//
+//  Created by Darrell Tan on 12/07/2025.
+//
+
+import Testing
+@testable import CPRMannequinAR
+
+struct CPRMannequinARTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
